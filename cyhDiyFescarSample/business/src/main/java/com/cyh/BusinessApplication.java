a4f9eaf1327484f2602c950bab262a0e7f4c5c99
yanhua.chen <yanhua.chen@bkjk.com>
2019-02-13 17:42:28 +0800

    业务调用开始/结束日志

--- cyhDiyFescarSample/business/src/main/java/com/cyh/BusinessApplication.java
+++ cyhDiyFescarSample/business/src/main/java/com/cyh/BusinessApplication.java
@@ -1,5 +1,7 @@
 package com.cyh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -8,10 +10,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class BusinessApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BusinessApplication.class);
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("business-dubbo.xml");
         BusinessService businessService = (BusinessService) context.getBean("business");
+        LOGGER.info("业务调用开始");
         businessService.purchase("cyh", "lightSuite", 5);
+        LOGGER.info("业务调用结束");
     }
 
 }
